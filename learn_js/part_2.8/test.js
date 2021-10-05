8233faef9a8d0c020f333ed7c01085e4b2022b09
AndSak <andriysak80@gmail.com>
2021-10-05 16:47:40 +0300

    feature: add part_2.8

---
+++ learn_js/part_2.8/test.js
@@ -0,0 +1,17 @@
+'use strict'
+// task 1
+let a=8, b=3;
+console.log(++a);
+console.log(b++);
+console.log(a);
+console.log(b);
+// task 2
+let A=2;
+let X=1+(A*=2);
+console.log(A);
+console.log(X);
+// task 4
+let aa = prompt("Первое число?", 1);
+let bb = prompt("Второе число?", 2);
+alert(aa+bb);//not right 
+console.log(+aa + +bb);
