c9ffb6c05ca6e3b71f26643494a7ab4e5fcbe583
AndSak <andriysak80@gmail.com>
2021-10-06 15:03:43 +0300

    feature: part_2.11

---
+++ learn_js/part_2.11/script.js
@@ -0,0 +1,10 @@
+//task 6
+let age = prompt('input number', '');
+
+if (age >= 14 && age <= 90) { alert('you are right!') }
+else { alert('wrong') };
+
+//task 7
+if (age < 14 || age > 90) { alert('not right!') };
+
+//task 8
