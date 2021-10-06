3c7a70141b5b27da1adc4c1c2d8a416967fcd9c1
AndSak <andriysak80@gmail.com>
2021-10-06 18:58:46 +0300

    feature: add task 8

--- learn_js/part_2.11/script.js
+++ learn_js/part_2.11/script.js
@@ -1,5 +1,5 @@
 //task 6
-let age = prompt('input number', '');
+let age = prompt('input number',);
 
 if (age >= 14 && age <= 90) { alert('you are right!') }
 else { alert('wrong') };
@@ -7,4 +7,20 @@ else { alert('wrong') };
 //task 7
 if (age < 14 || age > 90) { alert('not right!') };
 
-//task 8
+//task 7.1
+
+if (!(age < 14 || age > 90)) { alert('great! it is a nice') }
+else { alert(`it's bad ${age}`) };
+
+// task 8
+let login = prompt('! input the login !',);
+let pass;
+if (login == 'admin')
+ {
+    pass = prompt('input password', '1234567890')
+    if (pass == 1) { alert('hello BOSS!') }
+     else if (pass == '' || pass == null) { alert('cancel') }
+      else { alert('password is not right') }
+ }
+else if (login == '' || login == null) { alert('cancel') }
+else { alert('i dont know you') };
