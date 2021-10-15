f7761b1640d37798f13efa62176f6355cb40f837
AndSak <andriysak80@gmail.com>
2021-10-15 21:52:32 +0300

    feature: add part_4.1 Object

---
+++ learn_js/part_4.1/script41.js
@@ -0,0 +1,28 @@
+//task 1
+'use strict'
+let user = {};
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+
+console.log(user.name);
+
+delete user.name;
+
+//task 2
+function isEmpty(obj) {
+
+    let schedule = {};
+    // schedule.morning = prompt('WHAT DO you do at 8.30 o`clock ? ', '')
+    // schedule.afternoon = prompt('WHAT DO you do at 13.30 o`clock ? ', '')
+    // schedule.evening = prompt('WHAT DO you do at 20.30 o`clock ? ', '')
+
+    for (let key in schedule) {
+        alert(key);
+        return false;
+    }
+    alert('nothing');
+    return true;
+}
+isEmpty();
+console.log(isEmpty());
