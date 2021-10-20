f8ef0c407a9109476512ff3765dd362e20869088
AndSak <andriysak80@gmail.com>
2021-10-20 16:47:19 +0300

    feature: add task2 function construction

--- learn_js/part_4.5/script45.js
+++ learn_js/part_4.5/script45.js
@@ -1,33 +1,57 @@
-// clone --- my test
-let data = {
-    name: 'Dodo',
-    age: 45,
-    born: 'Mexico',
-    go: function () { console.log('ky-ky') },
+// // clone --- my test
+// let data = {
+//     name: 'Dodo',
+//     age: 45,
+//     born: 'Mexico',
+//     go: function () { console.log('ky-ky') },
+// };
+// let obj = {};
+// for (let g in data) { obj[g] = data[g] };
+// console.log(obj);
+// console.log(data.go());
+
+// let house = {
+//     square: 100,
+//     age: 30,
+// }
+// let oldest = Object.assign({}, data, house);
+// console.log(oldest);
+
+// // function construction --- my test
+// function User(name, age, nat) {
+//     this.name = name;
+//     this.age = age;
+//     this.sayHi = function () { 
+//         console.log("my name is " + this.name + " & I`am " + this.age) };
+//     this.nat = nat;
+// }
+// let vasya = new User("Vasya", 30, "ua", 55, "ky-ky");
+// let peter = new User("Peter", '', "usa");
+
+// vasya.sayHi();
+// console.log(vasya);
+// console.log(peter);
+// peter.sayHi();
+
+//task 2
+function Calculator() {
+
+    this.read = function () {
+        this.a = +prompt('input a ', 1);
+        this.b = +prompt('input b ', 1);
+    };
+    this.sum = function () {
+        alert(`Summa ${this.a}+${this.b} equals... `);
+        return (this.a + this.b);
+    };
+    this.mul = function () {
+        alert(`Multiplication ${this.a}*${this.b} equals... `);
+        return (this.a * this.b);
+    };
 };
-let obj = {};
-for (let g in data) { obj[g] = data[g] };
-console.log(obj);
-console.log(data.go());
+let calculator11 = new Calculator();
 
-let house = {
-    square: 100,
-    age: 30,
-}
-let oldest = Object.assign({}, data, house);
-console.log(oldest);
+calculator11.read();
 
-// function construction --- my test
-function User(name, age, nat) {
-    this.name = name;
-    this.age = age;
-    this.sayHi = function () { console.log("my name is " + this.name + " & I`am " + this.age) };
-    this.nat = nat;
-}
-let vasya = new User("Vasya", 30, "ua", 55, "ky-ky");
-let peter = new User("Peter", '', "usa");
-
-vasya.sayHi();
-console.log(vasya);
-console.log(peter);
-peter.sayHi();
+alert("Sum=" + calculator11.sum());
+alert("Mul=" + calculator11.mul());
