1a7491294b0424f45b562ec27e2dd1b01d7e3116
AndSak <andriysak80@gmail.com>
2021-10-26 13:26:14 +0300

    featre: part_5.3  task1 STRING

---
+++ learn_js/part_5.3/script53.js
@@ -0,0 +1,15 @@
+//task 1
+function ucFirst(word) {
+    if (word) {
+        word = word[0].toUpperCase() + word.slice(1);
+        console.log(word)
+    }
+    else return;
+};
+ucFirst("gogi");
+ucFirst("");
+ucFirst("pupsik");
+
+//task 2
+
+
