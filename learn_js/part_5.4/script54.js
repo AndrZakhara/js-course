dffd52458a09bd169c09d6f8ab05560cbf180c93
AndSak <andriysak80@gmail.com>
2021-10-28 14:58:16 +0300

    feature: add part_5.4 task2

--- learn_js/part_5.4/script54.js
+++ learn_js/part_5.4/script54.js
@@ -1,8 +1,25 @@
-//task 2
-let styles = ["jaz", "blues"];
-styles.push("rock", 'mix', 'opera', 'latino', 'valse', 'relax',);
-let len = Math.floor(styles.length / 2);
-styles[len] = ("classical");
-console.log(styles.shift());
-styles.unshift("reggi", "rep");
-console.log(styles);
+// //task 2
+// let styles = ["jaz", "blues"];
+// styles.push("rock", 'mix', 'opera', 'latino', 'valse', 'relax',);
+// let len = Math.floor(styles.length / 2);
+// styles[len] = ("classical");
+// console.log(styles.shift());
+// styles.unshift("reggi", "rep");
+// console.log(styles);
+
+// task 3
+function sumInput() {
+    let arrSum = [];
+    for (let i = 0; ; i++) {
+        let a = prompt('input number',);
+        if (!isNaN(a) && a !== null && a !== '') arrSum[i] = a;
+        else break;
+    };
+    let sum = 0;
+    for (let key of arrSum) { sum = sum + +key; };
+    console.log(arrSum);
+    return sum;
+};
+alert(sumInput());
+
+
