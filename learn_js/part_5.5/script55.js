3c314d7140bb1b36209f69f20bd0f10a689062ce
AndSak <andriysak80@gmail.com>
2021-11-03 23:20:54 +0200

    feature: add task 4, task 5 CONCAT

--- learn_js/part_5.5/script55.js
+++ learn_js/part_5.5/script55.js
@@ -21,15 +21,31 @@
 // console.log(filterRange(arr, 3, 6));
 // console.log(arr);
 
-// task 3
-let arr = [2, 5, 11, 3, 8, 1, 4, 6];
+// // task 3
+// let arr = [2, 5, 11, 3, 8, 1, 4, 6];
 
-function filterRangeInPlace(arr, a, b) {
+// function filterRangeInPlace(arr, a, b) {
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < a || arr[i] > b) { 
-            arr.splice(i, 1); i-- };
-    };
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < a || arr[i] > b) { 
+//             arr.splice(i, 1); i-- };
+//     };
+// };
+// filterRangeInPlace(arr, 5, 7);
+// console.log(arr);
+
+// task 4
+let arr = [0, 5, -2, 7, -1];
+arr.sort(function (a, b) { return (b - a) });
+console.log(arr);
+
+// task 5
+let arr = ["HTML", "JavaScript", "CSS", "PITON"];
+function copySorted(arr) {
+    let nArr = arr.concat();
+    nArr.sort();
+        console.log(nArr);
+     return;
 };
-filterRangeInPlace(arr, 5, 7);
+copySorted(arr);
 console.log(arr);
