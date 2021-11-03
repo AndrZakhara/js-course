e699a5eb0cb42053bfde5af6f284d81d4a09d11c
AndSak <andriysak80@gmail.com>
2021-11-03 20:27:37 +0200

    feature: add task 2, task 3;  FILTER

--- learn_js/part_5.5/script55.js
+++ learn_js/part_5.5/script55.js
@@ -1,14 +1,35 @@
 //task 1
-function camelize(str) {
+// function camelize(str) {
 
-    let arr = str.split('-');   // console.log(arr);
+//     let arr = str.split('-');   // console.log(arr);
 
-    let arr1 = arr.map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1));
+//     let arr1 = arr.map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1));
 
-    let arr2 = arr1.join('');
-   // return arr2;
+//     let arr2 = arr1.join('');
+//     return arr2;
+// };
+// console.log(camelize("list-style-image"));
+// console.log(camelize("background-color"));
+// console.log(camelize("-webkit-transition"));
+
+// // task 2
+// function filterRange(arr, a, b) {
+//     return arr.filter(item => +item >= a && +item < b);
+// };
+// let arr = [5, 3, 1, 9, 4, 6];
+
+// console.log(filterRange(arr, 3, 6));
+// console.log(arr);
+
+// task 3
+let arr = [2, 5, 11, 3, 8, 1, 4, 6];
+
+function filterRangeInPlace(arr, a, b) {
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < a || arr[i] > b) { 
+            arr.splice(i, 1); i-- };
+    };
 };
-console.log(camelize("list-style-image"));
-console.log(camelize("background-color"));
-console.log(camelize("-webkit-transition"));
-
+filterRangeInPlace(arr, 5, 7);
+console.log(arr);
