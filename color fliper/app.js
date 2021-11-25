28921079c4bb31076da0c66ef0f5915d85bc0a82
AndSak <andriysak80@gmail.com>
2021-11-25 14:42:01 +0200

    style: change indents

--- color fliper/app.js
+++ color fliper/app.js
@@ -1,26 +1,25 @@
 'use strict';
 
-let ground = document.querySelector('main'),
-    btn = ground.querySelector('#btn'),
-    nameBtn = ground.querySelector('span');
+const ground = document.querySelector('body'),
+  btn = document.getElementById('btn'),
+  nameBtn = document.querySelector('.color');
 
 const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
 
-btn = addEventListener('click', (event) => {
-    event.preventDefault();
+  let color = '#';
 
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+    color += arr[getRandom()];
+  };
 
-        color += arr[getRandom()];
-    };
-
-    ground.style.backgroundColor = color;
-    nameBtn.textContent = color;
+  document.body.style.backgroundColor = color;
+  nameBtn.textContent = color;
 
 });
 
 function getRandom() {
-    return (Math.floor(Math.random() * 16));
+  return (Math.floor(Math.random() * 16));
 };
