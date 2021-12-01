3af0c8d0c509bc4d1a81456f1b1d3bab88b4be7e
AndSak <andriysak80@gmail.com>
2021-12-02 00:28:16 +0200

    refactor: add filter [a-z]

--- Polindrom/script.js
+++ Polindrom/script.js
@@ -1,18 +1,19 @@
 function palindrome(str) {
 
-    let arr = str.split('');
-    console.log(arr);
-    let count = 0;
+  let count = 0;
+  let arr = str.toLowerCase().split('').filter(char => /[a-z]/.test(char));
+  console.log(arr);
 
-    for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
 
-        if (arr[i] == arr[arr.length - 1 - i]) {
-           
-            count++;
-        }
+    if (arr[i] == arr[arr.length - 1 - i]) {
+      count++;
     }
-    if (count == arr.length) { return true } else { return false }
-};
+  }
 
-let result = palindrome("eye");
+  if (count == arr.length) return true
+  else return false;
+
+};
+let result = palindrome("eyE YE");
 console.log(result);
