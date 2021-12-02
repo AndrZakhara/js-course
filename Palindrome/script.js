3a53b32c16cdaf8629d7507db2134e2ade654794
AndSak <andriysak80@gmail.com>
2021-12-02 10:10:03 +0200

    fix: change branch name, add halfLength

--- Palindrome/script.js
+++ Palindrome/script.js
@@ -2,18 +2,19 @@ function palindrome(str) {
 
   let count = 0;
   let arr = str.toLowerCase().split('').filter(char => /[a-z]/.test(char));
-  console.log(arr);
+  // console.log(arr);
+  let halfLength = Math.floor(arr.length/2); 
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < halfLength; i++) {
 
     if (arr[i] == arr[arr.length - 1 - i]) {
       count++;
     }
   }
 
-  if (count == arr.length) return true
+  if (count == halfLength) return true
   else return false;
 
 };
-let result = palindrome("eyE YE");
+let result = palindrome("My age is 0,  67555 si ega ym.");
 console.log(result);
