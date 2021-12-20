f8aa6bb026a13da773a4bd8a6f197ea1ac19501d
AndSak <andriysak80@gmail.com>
2021-12-20 09:37:53 +0200

    feature: add REST in class

--- udemy/project2/js/script.js
+++ udemy/project2/js/script.js
@@ -133,13 +133,14 @@ window.addEventListener('DOMContentLoaded', () => {
     // add class , template
     const curse = 28;
     class CardTemplate {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
             this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
             this.curse = curse;
             this.crossCurse();
         }
@@ -148,8 +149,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            };
             element.innerHTML = `
-            <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -158,7 +164,6 @@ window.addEventListener('DOMContentLoaded', () => {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-            </div>
             `;
             this.parent.append(element);
         }
@@ -172,7 +177,7 @@ window.addEventListener('DOMContentLoaded', () => {
         овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной
         ценой и высоким качеством!`,
         9,
-        '.menu .container',
+        ".menu .container",
     ).render();
 
     new CardTemplate(
@@ -183,7 +188,7 @@ window.addEventListener('DOMContentLoaded', () => {
         и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода
         в ресторан!`,
         15,
-        ".menu .container"
+        ".menu .container",
     ).render();
 
     new CardTemplate(
@@ -194,6 +199,6 @@ window.addEventListener('DOMContentLoaded', () => {
         продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное
         количество белков за счет тофу и импортных вегетарианских стейков.`,
         11,
-        ".menu .container"
+        ".menu .container",
     ).render();
 });
