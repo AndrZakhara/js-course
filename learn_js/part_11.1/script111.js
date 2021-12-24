52181c355f76b76fdc622f68fc3352a6768a70fe
AndSak <andriysak80@gmail.com>
2021-12-24 17:32:54 +0200

    refactor: task 1 use Promise

--- learn_js/part_11.1/script111.js
+++ learn_js/part_11.1/script111.js
@@ -1,13 +1,41 @@
-//task 1 
-function start() { showCircle(500, 300, 200, callback) };
-let callback = div => {
-  div.classList.add('message-ball');
-  div.append('Hello, world!');
+// //task 1 
+// function start() { showCircle(500, 300, 200, callback) };
+
+// let callback = div => {
+//   div.classList.add('message-ball');
+//   div.append('Hello, world!');
+// };
+
+// function showCircle(cx, cy, radius, callback) {
+//   let div = document.createElement('div');
+//   div.style.width = 0;
+//   div.style.height = 0;
+//   div.style.left = cx + 'px';
+//   div.style.top = cy + 'px';
+//   div.className = 'circle';
+//   document.body.append(div);
+
+//   setTimeout(() => {
+//     div.style.width = radius * 2 + 'px';
+//     div.style.height = radius * 2 + 'px';
+
+//     div.addEventListener('transitionend', function handler() {
+//       div.removeEventListener('transitionend', handler);
+//       callback(div);
+//     });
+
+//   }, 500);
+// };
+
+////task 2 part_11.2
+function start() {
+  showCircle(500, 300, 200).then(div => {
+    div.classList.add('message-ball');
+    div.append("PROMISE!");
+  })
 };
 
-
-
-function showCircle(cx, cy, radius, callback) {
+function showCircle(cx, cy, radius) {
   let div = document.createElement('div');
   div.style.width = 0;
   div.style.height = 0;
@@ -16,14 +44,15 @@ function showCircle(cx, cy, radius, callback) {
   div.className = 'circle';
   document.body.append(div);
 
-  setTimeout(() => {
-    div.style.width = radius * 2 + 'px';
-    div.style.height = radius * 2 + 'px';
+  return new Promise( resolve => {
+    setTimeout( () => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
 
-    div.addEventListener('transitionend', function handler() {
-      div.removeEventListener('transitionend', handler);
-      callback(div);
-    });
-
-  }, 500);
+      div.addEventListener('transitionend', function handler() {
+        div.removeEventListener('transitionend', handler);
+        resolve(div);
+      });
+    }, 0 )
+  })
 };
