bbb0dfa63566a105811f0afef247ebc34b33c1f6
AndSak <andriysak80@gmail.com>
2021-12-29 12:22:08 +0200

    feature: add JSON.parse

--- udemy/practic_5/script.js
+++ udemy/practic_5/script.js
@@ -1,19 +1,21 @@
 ////change USD
-const inputUAH = document.querySelector("#UAH");
-const inputUSD = document.querySelector("#USD");
+const inputUAH = document.querySelector("#uan");
+const inputUSD = document.querySelector("#usd");
 
 inputUAH.addEventListener('input', () => {
-    const request = new XMLHttpRequest();
+  const request = new XMLHttpRequest();
 
-    request.open("GET", "current.json");
-    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
-    request.send();
+  request.open("GET", "current.json");
+  request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  request.send();
 
-    request.addEventListener("readystatechange", () => {
-        if(request.readyState === 4 && request.status === 200){
-            console.log(request.response);
-
-        }
-    });
-
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const data = JSON.parse(request.response);
+      inputUSD.value = + inputUAH.value / data.current.usd;
+    } else {
+      inputUSD.value = " something  don't so! ";
+    }
+  });
 });
