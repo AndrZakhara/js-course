b86a53fc9fe344ba52cefca497b8d478c9fe1ac1
AndSak <andriysak80@gmail.com>
2022-01-21 18:38:29 +0200

    refactor: replacement XMLHttp => fetch

--- udemy/project2/js/script.js
+++ udemy/project2/js/script.js
@@ -289,8 +289,4 @@ window.addEventListener('DOMContentLoaded', () => {
     }, 5000);
   }
 
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json));
-
 });
